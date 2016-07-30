efb54c3375276d597461dd943346e67b94afc1e3
facmarques <facmarques@campus.fct.unl.pt>
2016-07-30 17:44:07 +0100

    Command now working don't know why it started working only did clone cleanups.

--- include/imau_control/Arm_interface.hpp
+++ include/imau_control/Arm_interface.hpp
@@ -71,22 +71,6 @@ namespace ARM
             std::vector<JointData> j_state_data;
             std::vector<JointData> j_cmd_data;
 
-            // Actuator and joint space variables - raw data:
-            // The first actuator/joint are coupled through a reducer.
-            // The last two actuators/joints are coupled through a differential.
-            double a_curr_pos[3]; // Size 3: One per actuator
-            double a_curr_vel[3];
-            double a_curr_eff[3];
-            std::vector<double> a_cmd_pos;
-
-            double j_curr_pos[3]; // Size 3: One per joint
-            double j_curr_vel[3];
-            double j_curr_eff[3];
-            double j_cmd_pos[3];
-
-
-
-
 
             ros::NodeHandle                               n;
             ros::NodeHandle                               nh;//for absolute names
@@ -101,17 +85,12 @@ namespace ARM
 
             // Interfaces
             hardware_interface::JointStateInterface    js_interface_;
-            //hardware_interface::JointModeInterface     jm_interface_;
-           // hardware_interface::EffortJointInterface   ej_interface_;
-            //hardware_interface::VelocityJointInterface vj_interface_;
             hardware_interface::PositionJointInterface pj_interface_;
 
             ros::Time                                       last_time;
             ros::Timer non_realtime_loop_;
 
-            bool                                          waiting_motorMessage;
-            bool                                          handleMotorRequestFLAG;
-            bool                                          handleMotorPermition;
+
             //std::vector<int>                            motorIDs;   
             //int                                         motorIDs[20]; 
             int                                           current_motor;  
@@ -124,21 +103,11 @@ namespace ARM
             ros::Subscriber                               arm_interface_sub;
 
             //FUNCTIONS
-            void constructor_global_fuction();
+            void constructor_global_function();
             void print_motors_info();
             void write();
             void read();
 
-            //control functions
-            bool init(hardware_interface::JointStateInterface&    js_interface,
-                      hardware_interface::PositionJointInterface& pj_interface);
-
-
-
-
-
-    	    //CALLBACKS
-//            void handleAMotorResponse(const sensor_msgs::JointState::ConstPtr& arm_request);
     };
 }
 
--- include/imau_control/Controller_hardware.hpp
+++ include/imau_control/Controller_hardware.hpp
@@ -52,42 +52,6 @@ namespace ARM
 
         private:
 
-            // TRANSMISSIONS
-           // RobotTransmissions robot_transmissions_;
-            //boost::scoped_ptr<TransmissionInterfaceLoader> transmission_loader_;
-
-            // Transmission interfaces
-            ActuatorToJointStateInterface    act_to_jnt_state; // For propagating current actuator state to joint space
-            JointToActuatorPositionInterface jnt_to_act_pos;   // For propagating joint position commands to actuator space
-
-            // Transmissions
-            //SimpleTransmission       sim_trans;
-            //DifferentialTransmission dif_trans;
-
-            // Actuator and joint space variables: wrappers around raw data
-            std::vector<ActuatorData> a_state_data; // Size 2: One per transmission
-            std::vector<ActuatorData> a_cmd_data;
-
-            std::vector<JointData> j_state_data;
-            std::vector<JointData> j_cmd_data;
-
-            // Actuator and joint space variables - raw data:
-            // The first actuator/joint are coupled through a reducer.
-            // The last two actuators/joints are coupled through a differential.
-            double a_curr_pos[3]; // Size 3: One per actuator
-            double a_curr_vel[3];
-            double a_curr_eff[3];
-            std::vector<double> a_cmd_pos;
-
-            double j_curr_pos[3]; // Size 3: One per joint
-            double j_curr_vel[3];
-            double j_curr_eff[3];
-            double j_cmd_pos[3];
-
-
-
-
-
             ros::NodeHandle                               n;
             ros::NodeHandle                               nh;//for absolute names
             std::vector<int>                              motorIDs;
@@ -99,39 +63,19 @@ namespace ARM
             sensor_msgs::JointState                       jointsStateActuator;
             std::vector<double>                           joint_position_command_;
 
-            // Interfaces
-         //   hardware_interface::JointStateInterface    js_interface_;
-            //hardware_interface::JointModeInterface     jm_interface_;
-           // hardware_interface::EffortJointInterface   ej_interface_;
-            //hardware_interface::VelocityJointInterface vj_interface_;
-           // hardware_interface::PositionJointInterface pj_interface_;
-
             ros::Time                                       last_time;
             ros::Timer non_realtime_loop_;
 
-            bool                                          waiting_motorMessage;
-            bool                                          handleMotorRequestFLAG;
-            bool                                          handleMotorPermition;
-            //std::vector<int>                            motorIDs;   
-            //int                                         motorIDs[20]; 
-            int                                           current_motor;  
-            int                                           motorCounter;   
+            int                                           motorCounter;
 
-            ros::Time                                     mTimer;  
-            ros::Time                                     timer_for_ellapse;      
-            ros::Publisher                                joints_pub;	
-	        
+            ros::Time                                     mTimer;
+
+            ros::Publisher                                joints_pub;
             ros::Subscriber                               arm_interface_sub;
 
             void standard_controller(std::string controller_name);
 
             void print_motors_info();
-
-
-
-
-
-
 
     	    //CALLBACKS
             void handleAMotorResponse(const sensor_msgs::JointState::ConstPtr& arm_request);
